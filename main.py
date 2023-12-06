76a8d27258986dcb005040a93917f75c1b2c2998
test <zxd_lngd@163.com>
2023-12-06 17:14:12 +0800

    modify default dataset

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_parameters():
     parser = argparse.ArgumentParser(description='STGCN')
     parser.add_argument('--enable_cuda', type=bool, default=True, help='enable CUDA, default as True')
     parser.add_argument('--seed', type=int, default=42, help='set the random seed for stabilizing experiment results')
-    parser.add_argument('--dataset', type=str, default='metr-la', choices=['metr-la', 'pems-bay', 'pemsd7-m'])
+    parser.add_argument('--dataset', type=str, default='pemsd7-m', choices=['metr-la', 'pems-bay', 'pemsd7-m'])
     parser.add_argument('--n_his', type=int, default=12) # 过去12个时刻
     parser.add_argument('--n_pred', type=int, default=3, help='the number of time interval for predcition, default as 3') # 预测3个时刻
     parser.add_argument('--time_intvl', type=int, default=5)
